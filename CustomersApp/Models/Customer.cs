a6e9c7ce24e790775167ce1bb44492617fa7b8a3
Olabamiji Oyetubo <oyetubobamiji@gmail.com>
2022-11-15 16:39:46 +0100

    first commit

---
+++ CustomersApp/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CustomersApp.Models;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    [DisplayName("First Name")]
+    public string FirstName { get; set; } = null!;
+    [DisplayName("Last Name")]
+    public string LastName { get; set; } = null!;
+}
---
+++ CustomersApp/Models/Demo1Context.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomersApp.Models;
+
+public partial class Demo1Context : DbContext
+{
+    public Demo1Context()
+    {
+    }
+
+    public Demo1Context(DbContextOptions<Demo1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            //        => optionsBuilder.UseSqlServer("Server=localhost;Database=Demo1;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true;");
+
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
